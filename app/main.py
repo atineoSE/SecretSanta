686d74af388b18ff674adeb55b4135dea2b66648
Adrian Tineo <adrian@adriantineo.com>
2024-12-14 19:42:56 +0100

    Add extra logging messages

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 import logging
 import sys
-from app.participants import participants, exclusions
-from app.randomizer import get_pairs
+
 from app.email_client import EmailClient
+from app.participants import exclusions, participants
+from app.randomizer import get_pairs
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -12,12 +13,17 @@ logging.basicConfig(
 
 if __name__ == "__main__":
     email_client = EmailClient()
+    logging.info(
+        f"Getting pairs for {len(participants)} participants with {len(exclusions)} exclusions."
+    )
     pairs = get_pairs(participants, exclusions)
     if not pairs:
         raise ValueError(
             "Could not find any matched pairs of giver-receiver. "
             + "Have you entered participants?"
         )
+
+    logging.info(f"Sending emails to {len(pairs)} participants.")
     for giver_name, receiver_name in pairs.items():
         email_client.send_email(
             giver_name=giver_name,
@@ -25,4 +31,7 @@ if __name__ == "__main__":
             receiver_name=receiver_name,
         )
     email_client.close()
-    logging.info(f"Sent Secret Santa emails to {len(pairs)} participants.")
+
+    logging.info(
+        f"SUCCESS: Secret Santa emails were sent to {len(pairs)} participants."
+    )
